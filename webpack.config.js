1a20f7f3db2997fb6d8a69cadc93fea4d9e88771
Wesley Magness <W.Magness17@gmail.com>
2015-09-15 20:42:12 -0400

    pasting in webpack script for diff

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,11 @@
 module.exports = {
-    // Entry point + files to be loaded
-    entry: getEntrySources(
-        ['./src/js/entry.js']
-    ),
-    // What to build and where
+    entry: getEntrySources(['./src/js/entry.js']),
     output: {
-        publicPath: 'http://localhost:8080',
+        publicPath: 'http://localhost:8080/',
         filename: 'build/bundle.js'
     },
-    // http://webpack.github.io/docs/configuration.html#devtool
     devtool: 'eval',
     module: {
-        // Used on code before transformed
         preLoaders: [
             {
                 test: /\.jsx?$/,
@@ -19,22 +13,17 @@ module.exports = {
                 loader: 'source-map'
             }
         ],
-        // Loaders http://webpack.github.io/docs/loaders.html
         loaders: [
-            // Find SCSS within src/
-            // SCSS -> SASS -> Autoprefixer -> CSS
             {
                 test: /\.scss$/,
                 include: /src/,
-                loaders : [
+                loaders: [
                     'style',
                     'css',
                     'autoprefixer?browsers=last 3 versions',
                     'sass?outputStyle=expanded'
                 ]
             },
-            // Find all images & optimize
-            // Base 64 encoded | > URL
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: [
@@ -42,9 +31,6 @@ module.exports = {
                     'img'
                 ]
             },
-            // Find all JS
-            // Ignore vendor
-            // Transpile with Babel
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
@@ -57,15 +43,11 @@ module.exports = {
     }
 };
 
-// ENV handler | Default = Development
-// This function enables the webpack development server (DVS)
-// and the hot swap loader to be considered entry-points to the script.
-// If the environment variable NODE_ENV isn’t set to production, then add these sources.
-
 function getEntrySources(sources) {
     if (process.env.NODE_ENV !== 'production') {
         sources.push('webpack-dev-server/client?http://localhost:8080');
         sources.push('webpack/hot/only-dev-server');
     }
+
     return sources;
 }
